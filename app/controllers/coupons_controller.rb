56e148ef6606e10174069cc558091cdf8a69c1c6
Roshanak Agahi <rosh.agahi@gmail.com>
2021-02-10 07:57:35 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -5,7 +5,7 @@ class CouponsController < ApplicationController
   
   def create 
     new_coupon = Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
-    redirect_to coupons_path(new_coupon)
+    redirect_to coupon_path(new_coupon)
   end 
   
   def index 
