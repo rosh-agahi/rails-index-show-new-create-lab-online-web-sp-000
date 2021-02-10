45360d2f9074c5044eabc8258dcecc5518214d1c
Roshanak Agahi <rosh.agahi@gmail.com>
2021-02-10 07:39:51 +0000

    Automatically backed up by Learn

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,15 @@
+class CouponsController < ApplicationController
+  
+  def new 
+  end 
+  
+  def create 
+    Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
+    redirect_to coupons_path
+  end 
+  
+  def index 
+    @coupons = Coupon.all
+  end
+  
+end
